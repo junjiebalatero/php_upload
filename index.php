ec3f437add9f52f50a9fed98ccb589149d5a30ab
margarito <margaritobalatero@gmail.com>
2018-04-23 12:31:04 +0800

    first commit

--- index.php
+++ index.php
@@ -1,335 +1,59 @@
+<?php
+	if (isset($_POST['upload'])){
+		$file_name = $_FILES['file']['name'];
+		$file_type = $_FILES['file']['type'];
+		$file_size = $_FILES['file']['size'];
+		$file_tem_Loc = $_FILES['file']['tmp_name'];
+		$file_store = "upload/".$file_name;
 
+		if (move_uploaded_file($file_tem_Loc, $file_store)){
+			echo "Files are Uploaded";
+		}
+	}
+?>
+
+<!DOCTYPE html>
 <!DOCTYPE html>
 <html>
 <head>
-   <title>ICC High School-Batch '85</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ICC High School- Batch '85</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<style>
-* {box-sizing: border-box}
-body {font-family: Verdana, sans-serif; margin:0}
-.mySlides {display: none}
-img {vertical-align: middle;}
+<!-- Optional theme -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-/* Slideshow container */
-.slideshow-container {
-  max-width: 1000px;
-  position: relative;
-  margin: auto;
-}
-
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  padding: 16px;
-  margin-top: -22px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-}
-
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.8);
-}
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  cursor: pointer;
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active, .dot:hover {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@-webkit-keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-
-@keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .prev, .next,.text {font-size: 11px}
-}
-</style>
+<!-- Latest compiled and minified JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
-<body bgcolor="#E6E6FA">
-
-  <center><h4>ICC High School-Batch '85</h4></center>
-
-  <div class="slideshow-container">
-
-<div class="mySlides fade">
-  <div class="numbertext">1 / 23</div>
-  <img src="junjie_batch85_pic/picedit1.jpg" style="width:100%">
-  <div class="text">pic1</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">2 / 23</div>
-  <img src="junjie_batch85_pic/pic2.jpg" style="width:100%">
-  <div class="text">pic2</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">3 / 23</div>
-  <img src="junjie_batch85_pic/pic3.jpg" style="width:100%">
-  <div class="text">pic3</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">4 / 23</div>
-  <img src="junjie_batch85_pic/pic4.jpg" style="width:100%">
-  <div class="text">pic4</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">5 / 23</div>
-  <img src="junjie_batch85_pic/pic5.jpg" style="width:100%">
-  <div class="text">pic5</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">6 / 23</div>
-  <img src="junjie_batch85_pic/pic6.jpg" style="width:100%">
-  <div class="text">pic6</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">7 / 23</div>
-  <img src="junjie_batch85_pic/pic7.jpg" style="width:100%">
-  <div class="text">pic7</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">8 / 23</div>
-  <img src="junjie_batch85_pic/pic8.jpg" style="width:100%">
-  <div class="text">pic8</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">9 / 23</div>
-  <img src="junjie_batch85_pic/pic9.jpg" style="width:100%">
-  <div class="text">pic9</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">10 / 23</div>
-  <img src="junjie_batch85_pic/pic10.jpg" style="width:100%">
-  <div class="text">pic10</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">11 / 23</div>
-  <img src="junjie_batch85_pic/pic11.jpg" style="width:100%">
-  <div class="text">pic11</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">12 / 23</div>
-  <img src="junjie_batch85_pic/pic12.jpg" style="width:100%">
-  <div class="text">pic12</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">13 / 23</div>
-  <img src="junjie_batch85_pic/pic13.jpg" style="width:100%">
-  <div class="text">pic13</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">14 / 23</div>
-  <img src="junjie_batch85_pic/pic14.jpg" style="width:100%">
-  <div class="text">pic14</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">15 / 23</div>
-  <img src="junjie_batch85_pic/pic15.jpg" style="width:100%">
-  <div class="text">pic15</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">16 / 23</div>
-  <img src="junjie_batch85_pic/pic16.jpg" style="width:100%">
-  <div class="text">pic16</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">17 / 23</div>
-  <img src="junjie_batch85_pic/pic17.jpg" style="width:100%">
-  <div class="text">pic17</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">18 / 23</div>
-  <img src="junjie_batch85_pic/pic18.jpg" style="width:100%">
-  <div class="text">pic18</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">19 / 23</div>
-  <img src="junjie_batch85_pic/pic19.jpg" style="width:100%">
-  <div class="text">pic19</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">20 / 23</div>
-  <img src="junjie_batch85_pic/pic20.jpg" style="width:100%">
-  <div class="text">pic20</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">21 / 23</div>
-  <img src="junjie_batch85_pic/pic21.jpg" style="width:100%">
-  <div class="text">pic21</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">22 / 23</div>
-  <img src="junjie_batch85_pic/pic22.jpg" style="width:100%">
-  <div class="text">pic22</div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">23 / 23</div>
-  <img src="junjie_batch85_pic/pic23.jpg" style="width:100%">
-  <div class="text">pic23</div>
-</div>
-
-<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-</div>
-<br>
-
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span>
-  <span class="dot" onclick="currentSlide(2)"></span>
-  <span class="dot" onclick="currentSlide(3)"></span>
-  <span class="dot" onclick="currentSlide(4)"></span>
-  <span class="dot" onclick="currentSlide(5)"></span>
-  <span class="dot" onclick="currentSlide(6)"></span>
-  <span class="dot" onclick="currentSlide(7)"></span>
-  <span class="dot" onclick="currentSlide(8)"></span>
-  <span class="dot" onclick="currentSlide(9)"></span>
-  <span class="dot" onclick="currentSlide(10)"></span>
-  <span class="dot" onclick="currentSlide(11)"></span>
-  <span class="dot" onclick="currentSlide(12)"></span>
-  <span class="dot" onclick="currentSlide(13)"></span>
-  <span class="dot" onclick="currentSlide(14)"></span>
-  <span class="dot" onclick="currentSlide(15)"></span>
-  <span class="dot" onclick="currentSlide(16)"></span>
-  <span class="dot" onclick="currentSlide(17)"></span>
-  <span class="dot" onclick="currentSlide(18)"></span>
-  <span class="dot" onclick="currentSlide(19)"></span>
-  <span class="dot" onclick="currentSlide(20)"></span>
-  <span class="dot" onclick="currentSlide(21)"></span>
-  <span class="dot" onclick="currentSlide(22)"></span>
-  <span class="dot" onclick="currentSlide(23)"></span>
-</div>
-<br>
-  <center><div style="background-color:lightblue">
-     <form action="upload.php" method="post"
-     enctype="multipart/form-data" action="upload.php">
-     <input type="file" name="file" id="file" size="35"><br>
-     <input type="submit" name="submit" id="submit" value="Submit">
-     </form>
-  </div></center>
-
-<!-- //////////////////////////////////// -->
-
-
-     <!--    <div style="display: table-cell;"> Right </div> -->
-    </div>
-</div>
-
-<br>
-  <script>
-  var slideIndex = 1;
-  showSlides(slideIndex);
-
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-  }
-  </script>
-<script src="https://www.shoutbox.com/chat/chat.js.php"></script>
-<script> var chat = new Chat(10680, "margie");</script>
+<body style="background-color:powderblue;">
+<center>	<form action = "?" method = "POST" enctype = "multipart/form-data">
+		<label>Uploading Files</label>
+		<p><input type="file" name="file"></p>
+		<p><input type="submit" name="upload" value = "Upload Image"></p>
+	</form>
+</center>
 </body>
 </html>
 
+<?php
+
+	$folder = "upload/";
+
+	if (is_dir($folder)){
+
+		if ($open = opendir($folder)){
+
+			while (($file = readdir($open)) !=false){
+
+				if ($file =='.' || $file =='..') continue;
+
+				echo ' <img src ="upload/'.$file.'" width = "150" height = 150 >';
+				}
+				closedir($open);
+			}
+
+		}
 
 
-        
-
+?>
